a4e87fa4f0906d2cb0cab49a3426670bfee8a866
Fatih Cambel <cambelfatih01@gmail.com>
2021-03-16 23:24:43 +0300

    Update find the most recurring number in a two-dimensional array.c

--- find the most recurring number in a two-dimensional array.c
+++ find the most recurring number in a two-dimensional array.c
@@ -1,67 +1,516 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+
 
 int main()
 {
-    int tekrarsayisi=1,temp,tkredensayi=0,j,i;
-    int m,a,n;
-    printf("dizinin satir sayisini girin");
-    scanf("%d",&m);
-    printf("dizinin sütun sayisini girin");
-    scanf("%d",&n);
-    int dizi[m][n];
-    srand(time(NULL));
-
-//**********************************
-
-
-    for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-    dizi[i][j] = rand()%m*n;
-            }
-    }
-
-
-    for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-    printf(" %5d ",dizi[i][j]);
-            }
-            printf("\n");
-    }
-
-
-
-//************************************
-
-
-for(a=0 ; a<m ; a++){
-        temp=0;
-
-
-    for(i=0; i<n; i++)
+    int satirsayisi=10,i,j,k,n,a=0,bosluk,boslu,bos;
+	printf("NOTE: This number is directly proportional to the letter size.");
+    printf("\nPlease enter a number greater than or equal to 10.");
+    printf("\n");
+    scanf("%d",&satirsayisi);
+       for(i=0; i<satirsayisi; i++)
     {
-        tekrarsayisi=1;
-      if(dizi[a][i]!=-9){
-
-
-        for(j=i+1; j<n; j++)
-            if(dizi[a][i]==dizi[a][j]){
-                tekrarsayisi++;
-                dizi[a][j]=-9;
-            }
-        if(tekrarsayisi>temp)
+        printf("  ");
+        printf("*");
+        if(i==0)
         {
-            tkredensayi=dizi[a][i];
-            temp=tekrarsayisi;
+            for(k=0; k<satirsayisi/2+5; k++)
+            {
+                printf("*");                      //FFFFFFFFFFFFFFFFF
+            }
+        }
+        if(i==satirsayisi/2-1)
+        {
+            for(n=0; n<satirsayisi/2+3; n++)
+            {
+                printf("*");
+            }
+        }
+
+        printf("\n");
+    }
+    printf("\n\n");
+
+
+
+
+
+
+
+    /*
+    ********
+    *      *
+    *      *
+    ********
+    *      *
+    *      *
+    *      *
+    */
+
+
+    for(i=0; i<satirsayisi-1; i++)
+    {
+        printf("  ");
+        printf("*");              //AAAAAAAAAAAAAAAAAAAAa
+        if(i==0 || i==satirsayisi/2-1)
+        {
+            for(n=0; n<satirsayisi/2+1; n++)
+            {
+                printf("*");
+            }
+        }
+        else
+        {
+            for(k=0; k<satirsayisi/2+1; k++)
+            {
+                printf(" ");
+            }
         }
 
 
+        printf("*\n");
+
     }
-    }
-    printf(" %d.indisi %d %d\n",a,temp,tkredensayi);
-}
+
+    printf("\n\n");
 
 
+
+
+
+    for(i=0; i<satirsayisi; i++)    //TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+    {
+        printf("  ");
+        if(i==0)
+        {
+            for(k=0; k<satirsayisi; k++)
+            {
+                printf("*");
+            }
+        }
+        else
+        {
+            for(n=0; n<satirsayisi/2; n++)
+            {
+                printf(" ");
+            }
+
+        }
+        printf("*\n");
+    }
+
+
+    printf("\n\n");
+
+
+
+
+    /*
+
+
+    *****
+    *   *
+    *****
+    ***
+     *
+     *
+     *
+    ***
+
+
+
+
+    */
+
+
+    for(i=0; i<3; i++)
+    {
+        for(k=0; k<satirsayisi/2; k++)
+        {
+            printf(" ");
+        }
+
+
+        for(k=0; k<3; k++)
+        {
+            printf("*");
+
+        }
+        printf("\n");
+
+
+    }
+    for(i=0; i<satirsayisi/2+4; i++)
+    {
+        for(k=0; k<satirsayisi/2+1; k++)
+        {
+            printf(" ");
+        }
+
+        printf("*\n");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    for (i=0; i<satirsayisi+1; i++)
+    {
+        printf("  ");//HHHHHHHHHHHHHHHHHHHHH
+        printf("*");
+        if (i==satirsayisi/2)
+        {
+            for (j=0; j<satirsayisi/2+5; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+        else
+        {
+            for (k=0; k<(satirsayisi/2)+4; k++)
+            {
+                printf(" ");
+            }
+            printf("*\n");
+        }
+    }
+    printf("\n\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    printf("\n\n");
+
+
+    /*
+
+      **********
+      *
+      *
+      *
+      **********
+         ***
+         ***
+
+     */
+
+
+
+
+    for(i=0; i<satirsayisi+1; i++)
+    {
+        printf("  ");
+
+        if(i==0 || i==satirsayisi)
+        {
+            printf(" ");
+
+            for(k=0; k<satirsayisi; k++)    //Ç Ç Ç ÇÇÇÇ
+            {
+                printf("*");
+            }
+        }
+        else
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(k=0; k<satirsayisi/2-3; k++)
+    {
+        for(j=0; j<satirsayisi/2+2; j++)
+        {
+            printf(" ");
+        }
+        printf("**");
+        printf("\n");
+
+    }
+
+
+
+    /*
+    ********
+    *      *
+    *      *
+    ********
+    *      *
+    *      *
+    *      *
+    */
+    printf("\n\n");
+
+    for(i=0; i<satirsayisi; i++)
+    {
+        printf("  ");
+        printf("*");              //AAAAAAAAAAAAAAAAAAAAa
+        if(i==0 || i==satirsayisi/2)
+        {
+            for(n=0; n<satirsayisi/2+4; n++)
+            {
+                printf("*");
+            }
+        }
+        else
+        {
+            for(k=0; k<satirsayisi/2+4; k++)
+            {
+                printf(" ");
+            }
+        }
+
+
+        printf("*\n");
+
+    }
+
+
+
+
+    /*
+
+
+     **       **
+     * *     * *
+     *  *   *  *         // MMMMMMM HARFÝ   3bosluk*\n2bosluk*\1bosluk*\n*
+     *   * *   *
+     *    *    *
+     *         *
+    **7bosluk**
+    *bosluk*5bosluk*bosluk*
+    *2bosluk*3bosluk*2bosluk*
+    *3bosluk*bosluk*3bosluk*
+    *4bosluk*4bosluk*
+    */
+
+
+    printf("\n\n");
+    bosluk=satirsayisi-1;
+
+
+
+    for(i=0; i<satirsayisi/2+3; i++)
+    {
+        printf("  ");
+        if(i<satirsayisi/2+1)
+        {
+            printf("*");
+
+
+            for(j=0; j<i; j++)
+            {
+                printf(" ");
+            }
+            if(i<satirsayisi/2)
+                printf("*");
+
+
+            for(j=0; j<bosluk; j++)                     //MMMMMMMMMMM
+            {
+                printf(" ");
+            }
+            printf("*");
+            bosluk=bosluk-2;
+
+
+            for(j=0; j<i; j++)
+            {
+                printf(" ");
+            }
+            printf("*\n");
+
+
+        }
+
+
+        else
+        {
+            printf("*");
+
+            for(n=0; n<satirsayisi+1; n++)
+            {
+                printf(" ");
+
+            }
+            printf("*\n");
+        }
+    }
+    /* BBBBBBBBBBBB
+       HARFI ASAGIDA
+
+
+
+       */
+    printf("\n\n");
+    for(i=0; i<satirsayisi+1; i++)
+    {
+        printf("  ");
+        if(i<satirsayisi/2+1)
+        {
+            printf("*");
+            if(i==0||i==satirsayisi/2)
+            {
+                for(j=0; j<satirsayisi/2+2; j++)
+                {
+                    printf("*");
+
+                }
+                printf("\n");
+            }
+            else
+            {
+                for(j=0; j<satirsayisi/2+2; j++)                              //BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+                {
+                    printf(" ");
+                }
+                printf("*\n");
+
+            }
+        }
+        else
+        {
+            printf("*");
+            if(i==satirsayisi)
+            {
+                for(j=0; j<satirsayisi/2+2; j++)
+                {
+                    printf("*");
+                }
+                printf("\n");
+            }
+            else
+            {
+                for(j=0; j<satirsayisi/2+2; j++)
+                {
+                    printf(" ");
+                }
+                printf("*\n");
+            }
+        }
+    }
+    /*
+    EEEEEEEEEEE HARFÝ
+    */
+    printf("\n\n");
+    for(i=0; i<satirsayisi+1; i++)
+    {
+        printf("  ");
+
+        if(i==0 ||  i==satirsayisi)
+        {
+            printf("*");
+
+            for(j=0; j<satirsayisi/2+3; j++)
+            {
+                printf("*");
+            }
+            printf("*\n");
+        }
+        else if(i==satirsayisi/2)
+        {
+            printf("*");
+            for(j=0; j<satirsayisi/2+1; j++)
+            {
+                printf("*");
+
+
+            }
+            printf("*\n");
+        }
+        else
+        {
+            printf("*\n");
+        }
+    }
+    /*  LLLLLLLLLLLLLLL
+       HARFÝÝÝ   */
+    printf("\n\n");
+    for(i=0; i<satirsayisi; i++)
+    {
+        printf("  ");
+        if(i==satirsayisi-1)
+        {
+            for(j=0; j<satirsayisi; j++)      //LLLLLLLLLLLLL
+            {
+                printf("*");
+            }
+        }
+        else
+        {
+            printf("*\n");
+        }
+    }
+    printf("\n\n");
     return 0;
 }
+
